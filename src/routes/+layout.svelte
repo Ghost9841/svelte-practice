b4ae6fa64ce2c039bd1c2d498ab522a6b59dd521
Ghost9841 <asubedi778@gmail.com>
2025-05-21 18:38:01 +0545

    Added some pages, figure out how it works, and used some things just like NextJS

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+
+<div class="main-container">
+    <slot/>
+
+</div>
+
+<style>
+    .main-container {
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+    }
+</style>
